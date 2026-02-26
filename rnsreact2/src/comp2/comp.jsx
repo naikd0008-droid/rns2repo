d5e2a92b5d9427196c17b230689efc9bfcff3aff
naikd0008-droid <naikd0008@gmail.com>
2026-02-26 11:43:56 +0530

    update

--- rnsreact2/src/comp2/comp.jsx
+++ rnsreact2/src/comp2/comp.jsx
@@ -1,5 +1,13 @@
 import './comp.css'
-import img1 from '../assets/icons8-group-16.png'
+import { FaBook } from "react-icons/fa6";
+import { CiStar } from "react-icons/ci";
+import { TbUsers } from "react-icons/tb";
+import { LuDollarSign } from "react-icons/lu";
+import { LuLayoutDashboard } from "react-icons/lu";
+import { FiLogOut } from "react-icons/fi";
+
+
+
 function Comp() {
  
 
@@ -12,22 +20,18 @@ function Comp() {
             <div className='a12'>Demo User</div></div>
         </div>
         <div className='middle'>
-            <div className='b1'></div>
-            <div className='d2'></div>
-            <div className='b3'></div>
-            <div className='b4'><img src={img1} alt="" img1/><b>Users</b></div>
-            <div className='b5'></div>
+            <button className='b1'><LuLayoutDashboard /> <b>Dashboard</b></button>
+            <button className='d2'><CiStar /> <b>Celebreties</b></button>
+            <button  className='b3'><FaBook /> <b>Bookings</b></button>
+            <button className='b4'><TbUsers /> <b>Users</b></button>
+            <button  className='b5'><LuDollarSign /> <b>Payments</b></button>
         </div>
         <div className='bottom'>
             <div className='d1'></div>
-            <div className='d2'>Logout</div>
+            <button className='d2'><FiLogOut/>Logout</button>
         </div>
     </div>
-
-
-
-
-
+    
     </>
   )
 }
